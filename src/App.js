61d272fe3ef4ba46abb7d5a489dba81f9f689092
Burak Selvi <burakselvi858@hotmail.com>
2021-08-03 19:42:20 +0300

    Revert back

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // const [iframeSrc, setIframeSrc] = useState('');
   function onClickIosApp () {
     const appUrl = 'https://apps.apple.com/tr/app/migros-sanal-market/id397585390?l=tr';
     const appName = 'sanalmarket://';
@@ -11,11 +13,10 @@ function App() {
   }
 
   function onClickAndroidApp () {
-    const now = new Date().valueOf();
     const appUrl = 'https://play.google.com/store/apps/details?id=com.inomera.sm';
     const appName = 'sanalmarket://';
+    // setIframeSrc(appName);
     setTimeout(() => {
-      if (new Date().valueOf() - now > 100) return;
       window.location.replace(appUrl);
     }, 25);
     window.location.replace(appName);
@@ -25,6 +26,7 @@ function App() {
     <div className="container">
       <button onClick={onClickIosApp}>Click for the IOS App</button>
       <button onClick={onClickAndroidApp}>Click for the ANDROID App</button>
+      {/* <iframe title="app" src={iframeSrc} id="l" width="1" height="1" style={{visibility: 'hidden'}}></iframe> */}
     </div>
   );
 }
