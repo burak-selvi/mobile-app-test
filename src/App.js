305ff0e32efd5aadcee91d24c3c1dda8cced5b6c
Burak Selvi <burakselvi858@hotmail.com>
2021-08-04 14:56:29 +0300

    Update 1.0.8

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {useState} from 'react';
 function App() {
   // const [iframeSrc, setIframeSrc] = useState('');
   const [text, setText] = useState('');
+  const [visibilityText1, setVisibilityText1] = useState('');
+  const [visibilityText2, setVisibilityText2] = useState('');
 
   function onClickIosApp () {
     const appUrl = 'https://apps.apple.com/tr/app/migros-sanal-market/id397585390?l=tr';
@@ -18,6 +20,13 @@ function App() {
   }
 
   function onClickAndroidApp () {
+    document.addEventListener('visibilitychange', () => {
+      if (document.visibilityState == "visible") {
+        setVisibilityText1("tab is active")
+      } else {
+        setVisibilityText2("tab is inactive")
+      }
+    })
     const appUrl = 'https://play.google.com/store/apps/details?id=com.inomera.sm';
     const appName = 'sanalmarket://';
     // setIframeSrc(appName);
@@ -31,7 +40,7 @@ function App() {
 
   return (
     <div className="container">
-      <div>Version: 1.0.7</div>
+      <div>Version: 1.0.8</div>
       <button onClick={onClickIosApp}>Click for the IOS App</button>
       <button onClick={onClickAndroidApp}>Click for the ANDROID App</button>
       <div>Bilgi: {text}</div>
@@ -39,6 +48,8 @@ function App() {
       <div>Bilgi - document-webkitHidden: {document.webkitHidden ? 'true' : 'false'}</div>
       <div>Bilgi - document-mozHidden: {document.mozHidden  ? 'true' : 'false'}</div>
       <div>Bilgi - document-msHidden: {document.msHidden  ? 'true' : 'false'}</div>
+      <div> active: {visibilityText1}</div>
+      <div> inactive: {visibilityText2}</div>
       {/* <iframe title="app" src={iframeSrc} id="l" width="1" height="1" style={{visibility: 'hidden'}}></iframe> */}
     </div>
   );
