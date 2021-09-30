fdccb27c3e4a223e035dec153bea318bfee061e6
Burak Selvi <burakselvi858@hotmail.com>
2021-10-01 01:18:20 +0300

    1.4.2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [visibilityText2, setVisibilityText2] = useState('');
   const [visibilityTime1, setVisibilityTime1] = useState('');
   const [visibilityTime2, setVisibilityTime2] = useState('');
+  const [isMobile, setMobile] = useState('');
   let fnc;
 
   useEffect(() => {
@@ -81,8 +82,73 @@ function App() {
       <div>Bilgi: {text} visibility: {visibility ? 'true' : 'false'}</div>
       <div> active: {visibilityText1} time: {visibilityTime1}</div>
       <div> inactive: {visibilityText2} time: {visibilityTime2}</div>
+      <div>Mobile: {navigator.userAgentData.mobile ? 'true' : 'false'}</div>
     </div>
   );
 }
 
 export default App;
+
+/*
+closeIcon = faTimesCircle;
+  popupVisible = true;
+  timer: any;
+
+  constructor(private platform: Platform, private _gtmService: GtmService) {}
+
+  @HostListener('document:visibilitychange', ['$event'])
+  onChangeVisibility(event: any): void {
+    if (event.target.hidden) {
+      clearTimeout(this.timer);
+    }
+  }
+
+  isPopupVisible(): boolean {
+    return this.popupVisible;
+  }
+
+  onInsideViewPort(): void {
+    if (this.popupVisible) {
+      this._gtmService.sendPageView({ event: 'ecommerceMobileAppView', eventType: 'Mobile App View' });
+    }
+  }
+
+  onClickPopupClose(): void {
+    this.popupVisible = false;
+  }
+
+  onClickOpenApp(): void {
+    const url = 'https://m2bf.adj.st?adj_t=xz9rbcr';
+    let appUrl: string, appName: string;
+    this._gtmService.sendPageView({ event: 'ecommerceMobileAppClick', eventType: 'Mobile App Click' });
+    this.onClickPopupClose();
+
+    if (this.platform.ANDROID) {
+      // utm parameters will be updated
+      appUrl =
+        'intent://?utm_source=sm#Intent;scheme=sanalmarket;package=com.inomera.sm;S.browser_fallback_url=https://play.google.com/store/apps/details?id=com.inomera.sm;end';
+      window.location.replace(appUrl);
+      return;
+    }
+
+    if (this.platform.IOS) {
+      appUrl = 'https://apps.apple.com/tr/app/migros-sanal-market/id397585390?l=tr';
+      appName = 'sanalmarket://';
+    }
+
+    window.location.replace(appName);
+    this.timer = setTimeout(() => {
+      if (this.platform.SAFARI) {
+        // only for safari
+        const msg = "App Store 'a Yönlendiriliyor...";
+        if (window.confirm(msg)) {
+          window.location.replace(appUrl);
+        } else {
+          window.close();
+        }
+      } else {
+        window.location.replace(appUrl);
+      }
+    }, 2000);
+  }
+*/
